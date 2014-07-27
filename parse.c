fbbc3d23daab03c3ebaef1d9b85d899f5a87ce28
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-07-27 00:48:15 -0400

    Sketch of parse function

--- parse.c
+++ parse.c
@@ -1,28 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-typedef union {
-    int integer;
-    char *symbol;
-} Data;
+#define MAX_SYMBOL_SIZE 32
+typedef enum {
+    false,
+    true,
+} bool;
 
 typedef enum {
     Number,
     Symbol,
+    Cons,
 } TypeTag;
+typedef struct _value Value;
+typedef struct _list List;
+typedef union _data Data;
 
-typedef struct {
-    union {
+struct _value {
+    union _data {
         int number;
+        List *list;
         char *symbol;
     } data;
     TypeTag tag;
-} Value;
+};
 
-typedef struct _list {
+struct _list {
     Value value;
-    struct _list *next;
-} List;
+    List *next;
+};
+
+List* makeList(Value value) {
+    // malloc a linked list, return the pointer
+    List *list = malloc(sizeof(List));
+    list->value = value;
+    list->next = NULL;
+    return list;
+}
 
 /* TODO: consider whether a pointer return type is correct here */
 Value* nth(int n, List *list) {
@@ -34,26 +47,93 @@ Value* nth(int n, List *list) {
         return NULL;
 }
 
+Value symbol(char *buf) {
+    return (Value) { (Data) buf, Symbol };
+}
+
 Value number(int n) {
-    return (Value) { n, Number };
+    return (Value) { (Data) n, Number };
 }
     
-
-List* parseList() {
-    return NULL;
+bool is_open_paren(char c) {
+    return c == 40;
 }
 
-List* makeList(Value value) {
-    // malloc a linked list, return the pointer
-    List *list = malloc(sizeof(List));
-    list->value = value;
-    list->next = NULL;
-    return list;
+bool is_close_paren(char c) {
+    return c == 41;
+}
+
+bool is_alpha(char c) {
+    return (c >= 65 && c <= 90) || (c >= 97 && c <= 122);
+}
+
+bool is_num(char c) {
+    return (c >= 30 && c <= 39);
+}
+
+enum ParseError {
+    None,
+    UnbalancedList,
+    MalformedSymbol,
+};
+
+typedef struct _parseresult {
+    char *newcursor;
+    Value value;
+    enum ParseError error;
+} ParseResult;
+
+ParseResult parsesym(char *cursor) {
+    char *symbuf = malloc(MAX_SYMBOL_SIZE * sizeof(char));
+    int i;
+    for (i = 0; *cursor != ' '; cursor++, i++) 
+        symbuf[i] = *cursor;
+    while (*cursor == ' ') 
+        cursor++;
+    return (ParseResult) { cursor, symbol(symbuf) };
+}
+
+int numval(char c) {
+    return c - 30;
+}
+
+ParseResult parsenum(char *cursor) {
+    int acc = 0;
+    for (; is_num(*cursor); cursor++) 
+        acc = acc * 10 + numval(*cursor);
+    return (ParseResult) { cursor, number(acc), None };
+}
+
+// TODO: test this! forgot to TDD in my excitement
+ParseResult parselist(char *buf) {
+    List *list = makeList(number(0)); // FIXME: make a nil value?
+    ParseResult result; 
+    /* consume the initial paren given to parselist, recursively advance 
+     * through tokens */
+    char *cursor = buf;
+    while (!is_close_paren(*cursor)) {
+        if (is_open_paren(*cursor)) {
+            result = parselist(cursor); 
+            cursor = result.newcursor;
+            list->value = result.value;
+        } else if (is_alpha(*cursor)) {
+            result = parsesym(cursor);
+            cursor = result.newcursor;
+            list->value = result.value;
+        } else if (is_num(*cursor)) {
+            result = parsenum(cursor);
+            cursor = result.newcursor;
+            list->value = result.value;
+        }
+    }
+    return result = (ParseResult) { cursor, (Value) { (Data) list, Cons } };
 }
 
 /* the cons function adds a new value to the head of a linked list 
  * and returns a pointer to the new head of the list without 
  * mutating or invalidating your old pointer to the head of the list
+ * Is this a good idea? I'm not sure. Maybe we want our pointers to
+ * the old head of the list to be updated?
  */
 List* cons(Value value, List *oldHead) {
     List *newHead = makeList(value);
@@ -61,6 +141,7 @@ List* cons(Value value, List *oldHead) {
     return newHead;
 }
 
+void printList(List *l);
 void printValue(Value *v) {
     switch (v->tag) {
         case Number:
@@ -68,6 +149,9 @@ void printValue(Value *v) {
             break;
         case Symbol:
             printf("%s ", v->data.symbol);
+            break;
+        case Cons:
+            printList(v->data.list);
             break;
     }
 }
